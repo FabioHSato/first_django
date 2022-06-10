24c840732d49969bed2dab03f4649b26970baa76
fabio sato <fabio@csstecnologia.com.br>
2022-06-09 22:49:49 -0300

    primeira alteração

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello Word')
+
+
+def soma(request, num1, num2):
+    return HttpResponse('<h1>Soma dos Numeros {} + {} = {}<h1>'.format(num1, num2, num1 + num2))
